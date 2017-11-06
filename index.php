7f359e9cdcf1e07a2de5b710679ecef416782c36
shunxing-w <shunxing_w@163.com>
2017-11-06 11:26:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php
-
-$data = '';
-
-
-$response = array(
-    'code'  => 999,
-    'message' => 'argument error for request',
-    'data'  => true,
-);
-
-echo json_encode($response);
-?>
+true
