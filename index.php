7f49ed57e82976588fc1fe6bf1376a3ea2be0d00
shunxing-w <shunxing_w@163.com>
2017-11-06 10:50:29 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/11/6
+ * Time: 10:41
+ */
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+echo json_encode($arr);
